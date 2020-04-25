20589e71bafdeeda03bdab78a1fc55d9fc3554e1
Vladislav Khorev <vladislav.khorev@fishrungames.com>
2020-04-25 15:08:07 +0500

    Added 2 buttons

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Welcome to COVIDnector!
+        </h1>
+		<p>
+          Please let us know who you are
         </p>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about us
-        </a>
+		 <div className="Buttons">
+			<div className="ButtonRow">
+			<a className="btn btn-primary" href="#" role="button">I want to become a donor</a>
+			</div>
+			<div className="ButtonRow">
+			<a className="btn btn-primary btn-success" href="#" role="button">I represent a hospital</a>
+			</div>
+		</div>
       </header>
+	  
+	 
+
     </div>
   );
 }
